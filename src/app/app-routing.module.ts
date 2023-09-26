c14463f557e5862e73e38e6ec27338045cbccb17
saif64s <my@wo.rk>
2023-09-26 23:53:34 +0600

    unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { NewPostComponent } from './pages/new-post/new-post.component';
 import { NewStoryComponent } from './pages/new-story/new-story.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { SearchComponent } from './pages/search/search.component';
 import { StoriesComponent } from './pages/stories/stories.component';
 
 const routes: Routes = [
